45667190491bd2b273813e0df308dc7a0e8befc2
Подкуйко Руслан Андреевич <ruslanzippert@gmail.com>
2022-03-31 14:03:54 +0300

    update Informatika_1_JFX

--- Informatika_1_JFX/src/convertFiles/Converter.java
+++ Informatika_1_JFX/src/convertFiles/Converter.java
@@ -19,23 +19,35 @@ public class Converter {
         });
 
 
-
         return list;
     }
 
     public String convert(String fileText) throws IOException {
+        int max = 0;
+        int maxLength = 0;
         ArrayList<List<String>> lists = new ArrayList<>();
-        Arrays.stream(fileText.split("\n")).forEach(x-> {
+        Arrays.stream(fileText.split("\n")).forEach(x -> {
             lists.add(splitLine(x));
         });
 
+        for (int x = 0; x < lists.size(); x++) {
+            if (lists.get(x).size() > max) {
+                max = lists.get(x).size();
+            }
+            for (int y = 0; y < lists.get(x).size(); y++) {
+                if (lists.get(x).get(y).length() > maxLength) {
+                    maxLength = lists.get(x).get(y).length();
+                }
+            }
+        }
         StringBuilder builder = new StringBuilder();
-        for (int x = 0; x < 19; x++) {
+        for (int x = 0; x < max; x++) {
             for (int y = 0; y < lists.size(); y++) {
                 if (x >= lists.get(y).size()) {
-                    builder.append("\t\t\t");
+                    builder.append(addSpace("",maxLength));
                 } else {
-                    builder.append(lists.get(y).get(x) + "\t\t\t");
+                    builder.append(addSpace(lists.get(y).get(x),maxLength));
+                    System.out.println(lists.get(y).get(x).length());
                 }
             }
             builder.append("\n");
@@ -49,5 +61,14 @@ public class Converter {
         writer.write(doneText);
         writer.flush();
         writer.close();
+    }
+
+    private String addSpace(String string, int maxLength) {
+        StringBuilder builder = new StringBuilder(string);
+        int needCount = maxLength - string.length() + 3;
+        for (int x = 0; x < needCount; x++) {
+            builder.append(" ");
+        }
+        return builder.toString();
     }
 }
